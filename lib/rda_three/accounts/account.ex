3471440a20f01fa55eee930dafcc6b2b388c0123
saparatayev <sopa.97@mail.ru>
2023-11-05 10:50:34 +0500

    refactor Account schema

--- lib/rda_three/accounts/account.ex
+++ lib/rda_three/accounts/account.ex
@@ -2,6 +2,7 @@ defmodule RdaThree.Accounts.Account do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @optional_fields [:id, :inserted_at, :updated_at]
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "accounts" do
@@ -12,11 +13,15 @@ defmodule RdaThree.Accounts.Account do
     timestamps()
   end
 
+  defp all_fields() do
+    __MODULE__.__schema__(:fields)
+  end
+
   @doc false
   def changeset(account, attrs) do
     account
-    |> cast(attrs, [:email, :hash_password])
-    |> validate_required([:email, :hash_password])
+    |> cast(attrs, all_fields())
+    |> validate_required(all_fields() -- @optional_fields)
     |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "Must have the @ sign and no spaces")
     |> validate_length(:email, max: 160)
     |> unique_constraint(:email)
